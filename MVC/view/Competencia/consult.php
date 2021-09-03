43763c6ab7f847e33b3eddab50b8f76dd0563a9c
Daniel Brito <dannielebless@gmail.com>
2021-09-03 12:48:52 -0500

    Modificar CRUD Competencia y Resultado Apr
    Añadir la librería data-table según como se viene trabajando en el resto
    de consultas de los demás módulos.
    
    También organizar las acciones y el botón de registrar con modal.

--- MVC/view/Competencia/consult.php
+++ MVC/view/Competencia/consult.php
@@ -1,13 +1,68 @@
-<a type="button" id="modal" data-toggle="modal" data-target="#exampleModal" data-url="<?php echo getURL("Competencia", "Competencia", "getInsertModal", false, "ajax") ?>">
-    <div style="background-color: #FF8001;padding:5px;">
-        <p style="margin:0;">Añadir Competencia</p>
+<br>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">Competencias</li>
+    </ol>
+</nav>
+
+<!-- Mensaje Insertar -->
+<?php
+
+if (isset($_SESSION['mensaje'])) {
+?>
+    <div class="alert alert-success alert-dimissible fade show mt-4" id="alerta" role="alert">
+        <?= $_SESSION['mensaje'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+
+        </button>
     </div>
-</a>
-<table style="text-align: center;">
-    <thead>
-        <th>ID</th>
-        <th>Competencia</th>
-        <th>Acciones</th>
+<?php
+    unset($_SESSION['mensaje']);
+}
+?>
+
+<!-- Mensaje Actualizar -->
+<?php
+
+if (isset($_SESSION['mensajeUpdate'])) {
+?>
+    <div class="alert alert-primary alert-dimissible fade show mt-4" id="alerta" role="alert">
+        <?= $_SESSION['mensajeUpdate'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+
+        </button>
+    </div>
+<?php
+    unset($_SESSION['mensajeUpdate']);
+}
+?>
+
+<!-- Mensaje Eliminar -->
+<?php
+
+if (isset($_SESSION['mensajeDelete'])) {
+?>
+    <div class="alert alert-danger alert-dimissible fade show mt-4" id="alerta" role="alert">
+        <?= $_SESSION['mensajeDelete'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+
+        </button>
+    </div>
+<?php
+    unset($_SESSION['mensajeDelete']);
+}
+?>
+
+<table class="table table-hover table-striped mt-3 table-light" id="tabla">
+    <thead class="thead-dark">
+        <tr>
+            <th>ID</th>
+            <th>Competencia</th>
+            <th>Acciones</th>
+        </tr>
     </thead>
 
     <tbody>
@@ -17,16 +72,19 @@
                 echo "<td>" . $comp['CompCod'] . "</td>";
                 echo "<td>" . $comp['CompDesc'] . "</td>";
                 echo "<td>
-                    <a id='modal' data-toggle='modal' style='cursor:pointer;' data-target='#exampleModal' data-url='" . getURL('Competencia', 'Competencia', 'getUpdateModal', array("CompCod"=>$comp['CompCod']), 'ajax') . "'>
-                        <svg id='edit' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 445.08 444.8' style='height:15px'>
-                            <title>Edit</title>
-                            <path class='cls-1' d='M391.2,219.75,149.57,461.37l-99,11s-22.72,1.25-23.14-22.93L38.93,349.8,280.55,108.18Z' transform='translate(-27.46 -27.6)'/>
-                            <path class='cls-1' d='M299.52,88.59,411.16,200.22l49.2-49.2s27.41-27.41,0-59.31L408.08,39.44s-27.88-26.63-58.94,0Z' transform='translate(-27.46 -27.6)'/>
-                        </svg>
-                    </a>
+                    <button  id='editarModal' data-url='" . getUrl("Competencia", "Competencia", "getUpdateModal", array("CompCod"=>$comp['CompCod']), "ajax") .
+                    "' type='button' class='btn btn-primary fas fa-edit ml-2 mr-2'></button>
                 </td>";
             echo "</tr>";
         }
         ?>
     </tbody>
 </table>
+
+<div class="row">
+    <div class="col-md-3">
+        <button type="button" id="modal" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-url="<?php echo getUrl("Competencia", "Competencia", "getInsertModal", false, "ajax") ?>">
+            Registrar
+        </button>
+    </div>
+</div>
--- MVC/view/ResultadoAprendizaje/consult.php
+++ MVC/view/ResultadoAprendizaje/consult.php
@@ -1,13 +1,69 @@
-<a type="button" id="modal" data-toggle="modal" data-target="#exampleModal" data-url="<?php echo getURL("ResultadoAprendizaje", "ResultadoAprendizaje", "getInsertModal", false, "ajax") ?>">
-    <div style="background-color: #FF8001;padding:5px;">
-        <p style="margin:0;">Añadir Resultado de Aprendizaje</p>
+<br>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">Resultados de Aprendizaje</li>
+    </ol>
+</nav>
+
+<!-- Mensaje Insertar -->
+<?php
+
+if (isset($_SESSION['mensaje'])) {
+?>
+    <div class="alert alert-success alert-dimissible fade show mt-4" id="alerta" role="alert">
+        <?= $_SESSION['mensaje'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+
+        </button>
     </div>
-</a>
+<?php
+    unset($_SESSION['mensaje']);
+}
+?>
+
+<!-- Mensaje Actualizar -->
+<?php
+
+if (isset($_SESSION['mensajeUpdate'])) {
+?>
+    <div class="alert alert-primary alert-dimissible fade show mt-4" id="alerta" role="alert">
+        <?= $_SESSION['mensajeUpdate'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+
+        </button>
+    </div>
+<?php
+    unset($_SESSION['mensajeUpdate']);
+}
+?>
+
+<!-- Mensaje Eliminar -->
+<?php
+
+if (isset($_SESSION['mensajeDelete'])) {
+?>
+    <div class="alert alert-danger alert-dimissible fade show mt-4" id="alerta" role="alert">
+        <?= $_SESSION['mensajeDelete'] ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+
+        </button>
+    </div>
+<?php
+    unset($_SESSION['mensajeDelete']);
+}
+?>
+
 <table class="table table-hover table-striped mt-3 table-light" id="tabla">
     <thead class="thead-dark">
-        <th>ID</th>
-        <th>Resultado Aprendizaje</th>
-        <th>Competencia</th>
+        <tr>
+            <th>ID</th>
+            <th>Resultado Aprendizaje</th>
+            <th>Competencia</th>
+            <th>Acciones</th>
+        </tr>
     </thead>
 
     <tbody>
@@ -18,16 +74,19 @@
                 echo "<td>" . $resApr['ReApDesc'] . "</td>";
                 echo "<td>" . $resApr['CompDesc'] . "</td>";
                 echo "<td>
-                    <a id='modal' data-toggle='modal' style='cursor:pointer;' data-target='#exampleModal' data-url='" . getURL('ResultadoAprendizaje', 'ResultadoAprendizaje', 'getUpdateModal', array("ReApCod"=>$resApr['ReApCod']), 'ajax') . "'>
-                        <svg id='edit' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 445.08 444.8' style='height:15px'>
-                            <title>Edit</title>
-                            <path class='cls-1' d='M391.2,219.75,149.57,461.37l-99,11s-22.72,1.25-23.14-22.93L38.93,349.8,280.55,108.18Z' transform='translate(-27.46 -27.6)'/>
-                            <path class='cls-1' d='M299.52,88.59,411.16,200.22l49.2-49.2s27.41-27.41,0-59.31L408.08,39.44s-27.88-26.63-58.94,0Z' transform='translate(-27.46 -27.6)'/>
-                        </svg>
-                    </a>
+                    <button  id='editarModal' data-url='" . getUrl("ResultadoAprendizaje", "ResultadoAprendizaje", "getUpdateModal", array("ReApCod"=>$resApr['ReApCod']), "ajax") .
+                    "' type='button' class='btn btn-primary fas fa-edit ml-2 mr-2'></button>
                 </td>";
             echo "</tr>";
         }
         ?>
     </tbody>
 </table>
+
+<div class="row">
+    <div class="col-md-3">
+        <button type="button" id="modal" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-url="<?php echo getUrl("Competencia", "Competencia", "getInsertModal", false, "ajax") ?>">
+            Registrar
+        </button>
+    </div>
+</div>
