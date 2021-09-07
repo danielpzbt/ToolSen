012c01d8a32fccdd768f55df76a32f2e10726035
Juan Steban <juanguarinsteban@gmail.com>
2021-09-07 16:37:10 -0500

    Consulta mejorada en PQRSF Controller

--- GUI/controller/Pqrsf/PqrsfController.php
+++ GUI/controller/Pqrsf/PqrsfController.php
@@ -1,30 +1,36 @@
 <?php
 
-    include_once '../model/Pqrsf/PqrsfModel.php';
+include_once '../model/Pqrsf/PqrsfModel.php';
 
-    class PqrsfController{
+class PqrsfController
+{
 
-        public function postInsert(){
-            $obj = new PqrsfModel();
+    public function postInsert()
+    {
+        $obj = new PqrsfModel();
 
-            $pqrsf_tipo_cod=$_POST['pqrsf_tipo_cod'];
-            $pqrsf_nombres=$_POST['pqrsf_nombres'];
-            $pqrsf_apellidos=$_POST['pqrsf_apellidos'];
-            $pqrsf_correo=$_POST['pqrsf_correo'];
-            $pqrsf_observacion=$_POST['pqrsf_observacion'];
-            $id=$obj->autoincrement("t_pqrsf","cod_pqrsf");
+        $pqrsf_tipo_cod = $_POST['pqrsf_tipo_cod'];
+        $pqrsf_nombres = $_POST['pqrsf_nombres'];
+        $pqrsf_apellidos = $_POST['pqrsf_apellidos'];
+        $pqrsf_correo = $_POST['pqrsf_correo'];
+        $pqrsf_observacion = $_POST['pqrsf_observacion'];
+        $id = $obj->autoincrement("t_pqrsf", "cod_pqrsf");
 
-            $sql="INSERT INTO t_pqrsf VALUES($id,$pqrsf_tipo_cod,NULL,NULL,'$pqrsf_nombres','$pqrsf_apellidos','$pqrsf_correo',NOW(),'$pqrsf_observacion')";
-            $ejecutar=$obj->insert($sql);
-
-            if ($ejecutar) {
-                redirect("index.php");
-            }else{
-                echo "Ups, ha ocurrido un error"; 
-                
-            }
-
+        if ($_POST['pqrsf_tipo_cod'] == 5) {
+            $sql = "INSERT INTO t_pqrsf VALUES($id,$pqrsf_tipo_cod,NULL,2,'$pqrsf_nombres','$pqrsf_apellidos','$pqrsf_correo',NOW(),'$pqrsf_observacion')";
+        } else {
+            $sql = "INSERT INTO t_pqrsf VALUES($id,$pqrsf_tipo_cod,NULL,1,'$pqrsf_nombres','$pqrsf_apellidos','$pqrsf_correo',NOW(),'$pqrsf_observacion')";
         }
-        
+
+
+        $ejecutar = $obj->insert($sql);
+
+        if ($ejecutar) {
+            redirect("index.php");
+        } else {
+            echo "Ups, ha ocurrido un error";
+        }
     }
+}
+
 ?>
