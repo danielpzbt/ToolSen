018bdc113d7f2f5954adfbe32dbde8aa3fc236ea
Juan Steban <juanguarinsteban@gmail.com>
2021-09-08 19:33:37 -0500

    Detail en PQRSF

--- MVC/controller/Pqrsf/PqrsfController.php
+++ MVC/controller/Pqrsf/PqrsfController.php
@@ -6,11 +6,21 @@
         public function consult(){
             $obj = new PqrsfModel();
 
-            $sql="SELECT * FROM t_pqrsf";
+            $sql="SELECT p.cod_pqrsf, t.pqrsf_tipo_desc, p.usu_id, e.desc_estado, p.pqrsf_nombres, p.pqrsf_apellidos, p.pqrsf_correo, p.pqrsf_fech, p.pqrsf_observacion FROM t_pqrsf AS p, t_pqrsf_tipo AS t, t_estado AS e WHERE t.pqrsf_tipo_cod = p.pqrsf_tipo_cod AND e.id_estado = p.id_estado";
             $pqrsfconsult=$obj->consult($sql);
 
             include_once '../view/Pqrsf/consult.php';
 
+        }
+        public function getDetail(){
+            $obj = new PqrsfModel();
+
+            $cod_pqrsf =$_POST['id'];
+
+            $sql="SELECT pqrsf_observacion FROM t_pqrsf WHERE cod_pqrsf=$cod_pqrsf";
+            $pqrsfdetail=$obj->consult($sql);
+
+            include_once '../view/Pqrsf/detailModal.php';
         }
 
 
--- MVC/view/Pqrsf/detailModal.php
+++ MVC/view/Pqrsf/detailModal.php
@@ -2,14 +2,13 @@
 foreach ($pqrsfdetail as $pqrsf) {
 ?>
     <div class="modal-body">
-        <p class="mb-1 info">Obersevación del PQRSF</p>
+        <p class="mb-1  info">Obersevación del PQRSF</p>
         <div class="col-md-12">
-            <textarea class="form-control" rows="5" name="pqrsf_observacion" required><?php echo $pqrsf['pqrsf_observacion'] ?></textarea>
+            <textarea class="form-control" rows="5" name="pqrsf_observacion" readonly><?php echo $pqrsf['pqrsf_observacion'] ?></textarea>
         </div>
     </div>
     <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-        <button type="submit" class="btn btn-primary">Registrar</button>
     </div>
 <?php
 }
